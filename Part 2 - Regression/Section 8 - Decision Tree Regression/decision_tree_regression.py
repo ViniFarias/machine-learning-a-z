b94aa5e9bd28d2d17c5ec84a8c48daa3443aa71c
Vinicius Farias <vinifarias.vf@gmail.com>
2018-03-21 13:44:17 -0300

    Decision Tree Regression in Python

--- Part 2 - Regression/Section 8 - Decision Tree Regression/decision_tree_regression.py
+++ Part 2 - Regression/Section 8 - Decision Tree Regression/decision_tree_regression.py
@@ -1,3 +1,5 @@
+# Decision Tree Regression
+
 # Polynomial Regression
 
 # Importing the libraries
@@ -23,26 +25,20 @@ sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
 
 # Fitting the Regression Model to the dataset
-# Section to put the regressor
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state = 0)
+regressor.fit(X, y)
 
 # Predicting a new result
 y_pred = regressor.predict(6.5)
 
-# Visualising the Regression results
-plt.scatter(X, y, color = "red")
-plt.plot(X, regressor.predict(X), color = "blue")
-plt.title("Truth or Bluff (Regression Model)")
-plt.xlabel("Position level")
-plt.ylabel("Salary")
-plt.show()
-
 # Visualising the Regression results (for higher resolution and smoother curve)
-X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
 
 plt.scatter(X, y, color = "red")
 plt.plot(X_grid, regressor.predict(X_grid), color = "blue")
-plt.title("Truth or Bluff (Regression Model)")
+plt.title("Truth or Bluff (Decision Tree Regression)")
 plt.xlabel("Position level")
 plt.ylabel("Salary")
 plt.show()
